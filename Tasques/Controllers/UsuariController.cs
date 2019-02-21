9e4227bfab66267ae8ecebc7014b0a2ba9bc2f7a
Toni <Toni@DESKTOP-50DRDG6>
2019-02-21 18:25:54 +0100

    Codi inicial

---
+++ Tasques/Controllers/UsuariController.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using Tasques.Models;
+
+namespace Tasques.Controllers
+{
+    
+    public class UsuariController : Controller
+    {
+        private static TasquesEntities db = new TasquesEntities();
+        private static List<USERTASK> llistaUserTask = db.USERTASK.ToList();
+        private static List<TASKS> llistaTask = db.TASKS.ToList();
+        private static int idUser = 1;
+        // GET: Usuari
+        public ActionResult Index()
+        {
+       
+            return View();
+        }
+        [HttpPost]
+        public object getUserTasks()
+        {
+            foreach(var task in llistaUserTask)
+            {
+                var a = task.IDTASK;
+            }
+            var jsonSerialiser = new JavaScriptSerializer();
+            var json = jsonSerialiser.Serialize(llistaUserTask);
+            return json;
+        }
+        [HttpPost]
+        public object getTasks()
+        {
+            Dictionary<int,string> dict = new Dictionary<int,string>();
+            foreach (var t in llistaTask)
+            {
+                foreach(var l in llistaUserTask)
+                {
+                    if (idUser == l.IDUSER && t.IDTASK == l.IDTASK)
+                    {
+                        dict.Add(t.IDTASK, t.NAME);
+                    }
+                }
+               
+            }
+            return JsonConvert.SerializeObject(dict);
+        }
+    }
+}
---
+++ Tasques/Scripts/TaskScripts/userPage.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    buildTaskSelector();
+});
+
+
+function buildTaskSelector(){
+    $.ajax({
+        type: "GET",
+        url: "@Url.Action("getUserTasks")",
+        content: "application/json; charset=utf-8",
+        dataType: "json",
+        data: JSON.stringify(data),
+        success: function (d) {
+            if (d.success == true)
+                alert('Has arribat!!');
+            else { }
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            alert('Error!!');
+        }
+    });
+}
